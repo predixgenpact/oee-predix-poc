d30dae13e41a1cbd3107fd9092f01b33f2fa949a
Amit Kharka <amit.kharka1@genpact.com>
2017-01-31 19:18:28 +0530

    2nd Screen

--- public/scripts/modules/controllers/oee-all-value-streams-ctrl.js
+++ public/scripts/modules/controllers/oee-all-value-streams-ctrl.js
@@ -1,4 +1,4 @@
-define(['angular', '../app-module'], function (angular) {
+define(['angular', '../app-module'], function(angular) {
     'use strict';
 
     // Controller definition
@@ -6,18 +6,32 @@ define(['angular', '../app-module'], function (angular) {
         .controller('OeeAllValueStreamsCtrl', OeeAllValueStreamsCtrl);
     OeeAllValueStreamsCtrl.$inject = ['$scope', '$timeout', '$window'];
 
-    function OeeAllValueStreamsCtrl ($scope, $timeout, $window){   
+    function OeeAllValueStreamsCtrl($scope, $timeout, $window) {
         $scope.oeeAllValueStreamsObj = {
             filter: {
                 style: {
                     height: '5px',
                     overflow: 'hidden',
                 },
-                show: false
+                applyButton: true,
+                resetButton: true,
+                show: false,
+                sortByOptions: ['Constraints', 'Alphabetically'],
+                selectedOptions: '',
+                viewOptionConstrain: [{
+                    name: 'Constrain Only',
+                    isChecked: true
+                }, {
+                    name: 'Throughput',
+                    isChecked: true
+                }]
             },
             filterIcon: {
                 style: {
                     top: '50px',
+                    left: '50px',
+                    right: '50px',
+                    width: '80%'
                 },
                 show: false
             },
@@ -26,87 +40,169 @@ define(['angular', '../app-module'], function (angular) {
                     show: false
                 },
                 showOeeCol: true,
-                showThroughputCol: true 
+                showThroughputCol: true
             }
         }
 
-        $scope.toogleFilter = function (){
+        // show/hide filter
+        $scope.toogleFilter = function() {
             $scope.oeeAllValueStreamsObj.filter.show = !$scope.oeeAllValueStreamsObj.filter.show;
             $scope.oeeAllValueStreamsObj.table.showThroughputCol = !$scope.oeeAllValueStreamsObj.table.showThroughputCol;
             $scope.oeeAllValueStreamsObj.table.showOeeCol = !$scope.oeeAllValueStreamsObj.table.showOeeCol;
 
-            if( $scope.oeeAllValueStreamsObj.filter.show) {
+            if ($scope.oeeAllValueStreamsObj.filter.show) {
                 $scope.oeeAllValueStreamsObj.filter.style = {
                     height: '55px',
                     overflow: 'hidden'
                 };
                 $scope.oeeAllValueStreamsObj.filterIcon.style = {
-                    top: '100px'
+                    top: '100px',
+                    left: '50px',
+                    right: '50px',
+                    width: '80%'
                 };
-                $timeout(function (){
-                    $scope.oeeAllValueStreamsObj.filter.style.overflow = 'inherit';                  
+                $timeout(function() {
+                    $scope.oeeAllValueStreamsObj.filter.style.overflow = 'inherit';
                 }, 600);
-                $scope.oeeAllValueStreamsObj.filterContent.style.display = 'block';                    
             } else {
                 $scope.oeeAllValueStreamsObj.filter.style = {
                     height: '5px',
                     overflow: 'hidden'
                 };
                 $scope.oeeAllValueStreamsObj.filterIcon.style = {
-                    top: '50px'
+                    top: '50px',
+                    left: '50px',
+                    right: '50px',
+                    width: '80%'
                 };
-                $timeout(function (){
-                    $scope.oeeAllValueStreamsObj.filterContent.style.display = 'none';                    
-                }, 500);
-            } 
+            }
         }
 
+        $scope.applyFilter = function() {
+            $scope.oeeAllValueStreamsObj.filter.applyButton = true;
+            $scope.oeeAllValueStreamsObj.filter.resetButton = true;
+            if ($scope.oeeAllValueStreamsObj.filter.viewOptionConstrain[0]['isChecked']) {
+
+            }
+            if ($scope.oeeAllValueStreamsObj.filter.viewOptionConstrain[0]['isChecked']) {
+
+            }
+        }
+
+        $scope.resetFilter = function() {
+            $scope.oeeAllValueStreamsObj.filter.viewOptionConstrain = [{
+                name: 'Constrain Only',
+                isChecked: true
+            }, {
+                name: 'Throughput',
+                isChecked: true
+            }];
+            $scope.oeeAllValueStreamsObj.filter.applyButton = true;
+            $scope.oeeAllValueStreamsObj.filter.resetButton = true;
+        }
+
+        // enable apply & reset button
+        $scope.update = function(item) {
+            $scope.oeeAllValueStreamsObj.filter.applyButton = false;
+            $scope.oeeAllValueStreamsObj.filter.resetButton = false;
+        }
+
+
         // table Json
-        $scope.tree_data = [
-            {'Value Streams':"USA1", 'Availability': 9826675, 'Performance': 4545, 'Quality Rate': 318212000,'OEE': "UTC -5 to -10", 'Target': '-', 'Actual': '-', 'Delinquency': '-',
-            children:[
-                {'Value Streams':"USA2", 'Availability': 9826675, 'Performance': 4545, 'Quality Rate': 318212000,'OEE': "UTC -5 to -10", 'Target': '-', 'Actual': '-', 'Delinquency': '-',
-                    children: [
-                        {'Value Streams':"USA3", 'Availability': 9826675, 'Quality Rate': 318212000,'OEE': "UTC -5 to -10", 'Target': '-', 'Actual': '-', 'Delinquency': '-',
-                            children: [
-                                {'Value Streams':"USA4", 'Availability': 9826675, 'Performance': 4545, 'Quality Rate': 318212000,'OEE': "UTC -5 to -10", 'Target': '-', 'Actual': '-', 'Delinquency': '-'},
-                                {'Value Streams':"USA4", 'Availability': 9826675, 'Performance': 4545, 'Quality Rate': 318212000,'OEE': "UTC -5 to -10", 'Target': '-', 'Actual': '-', 'Delinquency': '-'},
-                                {'Value Streams':"USA4", 'Availability': 9826675, 'Performance': 4545, 'Quality Rate': 318212000,'OEE': "UTC -5 to -10", 'Target': '-', 'Actual': '-', 'Delinquency': '-'}
-                            ],
-                            icons: {
-                                iconLeaf: "fa fa-chevron-down",
-                                iconCollapse: "fa fa-chevron-up",
-                                iconExpand: "fa fa-chevron-down"
+        $scope.tree_data = [{
+                'Value Streams': "Rotating parts & Compressor Airfoils",
+                'Availability': "XX%",
+                'Performance': "XX%",
+                'Quality Rate': "XX%",
+                'OEE': "XX%",
+                'Target': '-',
+                'Actual': '-',
+                'Delinquency': '-',
+                children: [{
+                    'Value Streams': "Site Name",
+                    'Availability': "XX%",
+                    'Performance': "XX%",
+                    'Quality Rate': "XX%",
+                    'OEE': "XX%",
+                    'Target': '-',
+                    'Actual': '-',
+                    'Delinquency': '-',
+                    children: [{
+                        'Value Streams': "Mechine ##",
+                        'Availability': "XX%",
+                        'Performance': "XX%",
+                        'Quality Rate': "XX%",
+                        'OEE': "XX%",
+                        'Target': '-',
+                        'Actual': '-',
+                        'Delinquency': '-',
+                        children: [{
+                                'Value Streams': "Part # XXXX001",
+                                'Availability': "-",
+                                'Performance': "-",
+                                'Quality Rate': "-",
+                                'OEE': "-",
+                                'Target': "XXX",
+                                'Actual': "YYY",
+                                'Delinquency': "ZZZ"
+                            },
+                            {
+                                'Value Streams': "Part # XXXX002",
+                                'Availability': "-",
+                                'Performance': "-",
+                                'Quality Rate': "-",
+                                'OEE': "-",
+                                'Target': "XXX",
+                                'Actual': "YYY",
+                                'Delinquency': "ZZZ"
+                            },
+                            {
+                                'Value Streams': "Part # XXXX003",
+                                'Availability': "-",
+                                'Performance': "-",
+                                'Quality Rate': "-",
+                                'OEE': "-",
+                                'Target': "XXX",
+                                'Actual': "YYY",
+                                'Delinquency': "ZZZ"
                             }
-                        }
-                    ],
-                    icons: {
-                        iconLeaf: "fa fa-chevron-down",
-                        iconCollapse: "fa fa-chevron-up",
-                        iconExpand: "fa fa-chevron-down"
-                    }
-                }
-            ],
-            icons: {
-                iconLeaf: "-",
-                iconCollapse: "fa fa-chevron-up",
-                iconExpand: "fa fa-chevron-down"
-            }},
-            {'Value Streams':"USA000", 'Availability': 9826675, 'Performance': 4545, 'Quality Rate': 318212000,'OEE': "UTC -5 to -10", 'Target': '-', 'Actual': '-', 'Delinquency': '-',
-        icons: {
-                iconLeaf: '-'
-            }}
+                        ]
+                    }]
+                }]
+            },
+            {
+                'Value Streams': "Turbine Airfoils",
+                'Availability': 9826675,
+                'Availability': "XX%",
+                'Performance': "XX%",
+                'Quality Rate': "XX%",
+                'OEE': "XX%",
+                'Target': '-',
+                'Actual': '-',
+                'Delinquency': '-'
+            },
+            {
+                'Value Streams': "Assembly Test & MRO",
+                'Availability': 9826675,
+                'Availability': "XX%",
+                'Performance': "XX%",
+                'Quality Rate': "XX%",
+                'OEE': "XX%",
+                'Target': '-',
+                'Actual': '-',
+                'Delinquency': '-'
+            }
         ];
-            
-        // adding header in table
-        $timeout(function () {
+
+        // adding header in table once table is rendered in DOM
+        $timeout(function() {
             angular.element('.vs-table table thead').prepend('<tr> <th></th>\
            <th colspan="4"> Overall Equipment Effectiveness</th> <th colspan="3"> Throughput</th></tr>');
         }, 100);
 
-        $scope.tableHandler = function (branch) {
-            debugger;   
+        //
+        $scope.tableHandler = function(branch) {
+            //debugger;   
         }
-        
     }
 });
